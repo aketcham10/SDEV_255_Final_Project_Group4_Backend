c41239f37d0d9741db143a913a2e1a6f8924989c
aketcham10 <186063166+aketcham10@users.noreply.github.com>
2025-10-06 20:07:56 -0400

    Removed mongodb options

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 const uri = process.env.URI
-mongoose.connect(uri, { useNewUrlParser: true});
+mongoose.connect(uri);
 
 module.exports = mongoose
